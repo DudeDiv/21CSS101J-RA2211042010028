8c7184c622f10cf62c396769108859457c4afa33
DudeDiv <113873506+DudeDiv@users.noreply.github.com>
2022-09-27 13:20:11 +0530

    Add files via upload

---
+++ aoac.c
@@ -0,0 +1,9 @@
+# include<stdio.h>
+   int main() {
+    //area of circle
+      float radius;
+      printf("What's the radius?");
+      scanf("%f", &radius);
+      printf("Hmm, ok, area is : %f", 3.14*radius*radius);
+      return 0;
+}
