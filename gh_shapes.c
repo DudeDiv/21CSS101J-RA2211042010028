d70c257808ed698beeb5a90611e33fd9cbabb8ba
DudeDiv <113873506+DudeDiv@users.noreply.github.com>
2022-10-09 22:39:35 +0530

    Add files via upload

---
+++ gh_shapes.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+printf("~TRIANGLE:\n");
+printf("    #\n");
+printf("   ###\n");
+printf("  #####\n");
+printf(" #######\n\n\n");
+printf("~SQUARE:\n");
+printf(" 11111111\n");
+printf(" 1      1\n");
+printf(" 1      1\n");
+printf(" 1      1\n");
+printf(" 11111111\n\n\n");
+printf("~RECTANGLE:\n");
+printf(" 00000000000000000\n");
+printf(" 0               0\n");
+printf(" 0               0\n");
+printf(" 0               0\n");
+printf(" 0               0\n");
+printf(" 0               0\n");
+printf(" 00000000000000000\n");
+return 0;
+}
