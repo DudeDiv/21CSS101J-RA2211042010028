13a9102f655e7708979ac08258964f439a19cd91
DudeDiv <113873506+DudeDiv@users.noreply.github.com>
2022-10-14 17:14:14 +0530

    Update return_value_3.c

--- return_value_3.c
+++ return_value_3.c
@@ -3,6 +3,7 @@ int myFunction(int x, int y, int z){
 return x+y+z;
 }
 int main(){
+  printf("Registration number : RA2211042010028\n");
 int result=myFunction(3,6,9);
 printf("Result is what it is : %d", result);
 return 0;
