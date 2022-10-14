8ff393f54b11e37687fd7266909a72a590a1f992
DudeDiv <113873506+DudeDiv@users.noreply.github.com>
2022-10-14 17:23:57 +0530

    Add files via upload

---
+++ ct1_question.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+//Time taken to reach Delhi from Ranchi.
+float speed, distance, time_taken;
+printf("Enter the distance (in kms) between Ranchi and Delhi : ");
+scanf("%f", &distance);
+printf("What was your average speed (in kmph)for the whole trip? Type here : ");
+scanf("%f", &speed);
+printf("Time taken (in mins) to reach Delhi from Ranchi = %f", time_taken=(distance/speed)*60);
+return 0;
+}
