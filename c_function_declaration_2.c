0fbc485a71a355d47a10ddff869b2df237565169
DudeDiv <113873506+DudeDiv@users.noreply.github.com>
2022-10-14 17:06:59 +0530

    Update c_function_declaration_2.c

--- c_function_declaration_2.c
+++ c_function_declaration_2.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int myFunction(int, int);
 int main(){
+  printf("Registration number : RA2211042010028\n");
 int result=myFunction(6,7);
 printf("Result is : %d", result);
 return 0;
