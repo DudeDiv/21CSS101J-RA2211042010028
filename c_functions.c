04c2f9fef68e44af9a705d0d934879a2e3b15e46
DudeDiv <113873506+DudeDiv@users.noreply.github.com>
2022-10-14 17:08:26 +0530

    Update c_functions.c

--- c_functions.c
+++ c_functions.c
@@ -3,6 +3,7 @@ void myFunction(){
 printf("I just got summoned!\n");
 }
 int main(){
+  printf("Registration number : RA2211042010028\n");
 myFunction();
 myFunction(); //A function can be called multiple times
 return 0;
